af0713cff42e2fb301ba674ff629b5eaa7771e04
zhou <polaris_zr@163.com>
2019-07-18 17:44:15 +0800

    本地Git库

---
+++ gitdemo01/src/main/java/entity/User.java
@@ -0,0 +1,28 @@
+package entity;
+
+/**
+ * @program: git-demo
+ * @description:
+ * @author: Ms.Zhou
+ * @create: 2019-07-18 16:55
+ **/
+public class User {
+    private String id;
+    private String uname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+}
